afef8b16c037959b4e5236929f6fda24a1e62d32
RuVMakarov <RuVMakarov@IMM-14-512-17>
2025-01-09 14:18:41 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Globalization;
+
+class Program
+{
+    static void Main()
+    {
+        Timer t = new Timer(1,2,3);
+        Counter c = new Counter(5);
+        c = (Counter)t;
+        Console.WriteLine(c);
+    }
+}
+
+
+class Timer
+{
+    public int hours;
+    public int minutes;
+    public int seconds;
+
+    public Timer(int h, int m, int s)
+    {
+        hours = h;
+        minutes = m;
+        seconds = s;
+    }
+
+    public static explicit operator Timer(Counter c)
+    {
+        return new Timer(c/3600, c % 3600/60, c%60);
+    }
+
+}
+
+class Counter
+{
+    public int seconds;
+
+    public Counter(int s)
+    {
+        seconds = s;
+    }
+
+    public static explicit operator Counter(Timer t)
+    {
+        int s = t.hours * 3600 + t.minutes * 60 + t.seconds;
+        return new Counter(s);
+    }
+    public static implicit operator int(Counter c)
+    {
+        return c.seconds;
+    }
+
+    public override string ToString()
+    {
+        return seconds.ToString();
+    }
+}
